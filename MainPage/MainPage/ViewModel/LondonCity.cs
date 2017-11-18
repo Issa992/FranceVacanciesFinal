b2482156bcc0b36a3f76f385e26204a9a2e4f087
yaamuu <yaamu122@gmail.com>
2017-11-18 14:24:36 +0100

    Added City classes, dunno if they are good

---
+++ MainPage/MainPage/ViewModel/LondonCity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainPage.Models;
+
+namespace MainPage.ViewModel
+{
+    class LondonCity
+    {
+        private ObservableCollection<Appartment> ApartmentList;
+
+        public LondonCity()
+        {
+            Catalog catalog = new Catalog();
+            catalog.AddLondonApartment("Eye", "The best Apartment in London", 2200,
+                true, true, false, "../assets/Photos/interior_flat_house_creative_white_11104_3840x2400.jpg");
+            ApartmentList = catalog.LondonApartments;
+
+        }
+    }
+}
